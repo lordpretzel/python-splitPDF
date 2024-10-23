01348112bf9f6248a05e446f5096e19cc669f68f
Boris Glavic <lordpretzel@gmail.com>
2024-10-23 16:30:08 -0500

    split

--- flake.nix
+++ flake.nix
@@ -15,38 +15,44 @@
           };
 
           requirements-txt = "${self}/requirements.txt";
-          requirements-as-text = builtins.readFile requirements-txt;
+          requirements-as-text = "";
 
-          development-requirements-as-text = requirements-as-text +
-                                             ''
+          development-requirements-as-text = ''
 pip
 python-lsp-server[all]
 rich-cli
 mypy
-'';
+''
+            ;
+
+          extrapackages = with pkgs; [
+            pdftk
+          ];
 
           python = "python310";
-          
+
           # python environment
-          mypython = 
+          mypython =
             mach-nix.lib."${system}".mkPython {
               inherit python;
               requirements = requirements-as-text;
             };
+
+          deps = [ mypython ] ++ extrapackages;
 
           mydevpython =
             mach-nix.lib."${system}".mkPython {
               inherit python;
               requirements = development-requirements-as-text;
             };
-          
+
+          devdeps = [ mydevpython ] ++ extrapackages;
+
           # Utility to run a script easily in the flakes app
           simple_script = name: add_deps: text: let
             exec = pkgs.writeShellApplication {
               inherit name text;
-              runtimeInputs = with pkgs; [
-                mypython
-              ] ++ add_deps;
+              runtimeInputs = deps ++ add_deps;
             };
           in {
             type = "app";
@@ -58,7 +64,7 @@ mypy
           pyscript = "${self}/${script-name}";
           package-version = "1.0";
           package-name = "${script-base-name}-${package-version}";
-          
+
         in with pkgs;
           {
             ###################################################################
@@ -68,19 +74,19 @@ mypy
               splitPDF = stdenv.mkDerivation {
                 name="${package-name}";
                 src = ./.;
-                
-                runtimeInputs = [ mypython ];
-                buildInputs = [ mypython ];
+
+                runtimeInputs = deps;
+                buildInputs = deps;
                 nativeBuildInputs = [ makeWrapper ];
                 installPhase = ''
                   mkdir -p $out/bin/
                   mkdir -p $out/share/
                   cp ${pyscript} $out/share/${script-name}
-                  makeWrapper ${mypython}/bin/python $out/bin/${script-base-name} --add-flags "$out/share/${script-name}" 
-                '';                
+                  makeWrapper ${mypython}/bin/python $out/bin/${script-base-name} --add-flags "$out/share/${script-name}"
+                '';
               };
             };
-            
+
             ###################################################################
             #                       running                                   #
             ###################################################################
@@ -95,10 +101,8 @@ mypy
             ###################################################################
             devShells.default = mkShell
               {
-                buildInputs = [
-                  mydevpython
-                ];
-                runtimeInputs = [ mydevpython ];
+                buildInputs = devdeps;
+                runtimeInputs = devdeps;
                 shellHook = ''
                   alias pip="${mydevpython}/bin/pip --disable-pip-version-check"
                   rich "[b white on black]Using virtual environment for [/][b white on red] ${package-name} [/][b white on black] with Python[/]
--- splitPDF.py
+++ splitPDF.py
@@ -1,16 +1,9 @@
-#!/Users/lord_pretzel/.pyenv/shims/python
-
-"""Splits an input pdf file into several given a list of splitting
-points (page numbers).
-"""
-
-__author__ = 'benhdj@cs.cmu.edu (Benjamin Han)'
+#!/usr/bin/env -S nix develop . --command python
 
 
 import sys
 import os
-
-from Quartz import CGPDFContextCreateWithFilename, CGPDFDocumentCreateWithProvider, CGDataProviderCreateWithFilename, CGRectMake
+import subprocess
 
 def Usage():
   print("""
@@ -41,50 +34,51 @@ if len(sys.argv) < 3:
   sys.exit(1)
 else:
   inputFN = sys.argv[1]
-  inputDoc = \
-    CGPDFDocumentCreateWithProvider(\
-    CGDataProviderCreateWithFilename(inputFN))
 
-  if inputDoc:
-    maxPages = inputDoc.getNumberOfPages()
+  if os.path.exists(inputFN):
+    try:
+      cmd = f"pdftk {inputFN} dump_data | grep NumberOfPages | awk '{{ print $2 }}' | tail -n 1"
+      ps = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+      result = ps.communicate()[0]
+      print(f"result was {result}")
+      maxPages = int(result)
+    except Exception:
+      cmd = f"pdfinfo {inputFN} | grep Pages | awk '{{ print $2 }}'"
+      ps = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+      result = ps.communicate()[0]
+      maxPages = int(result)
     print('%s has %d pages' % (inputFN, maxPages))
   else:
     sys.exit(2)
 
   try:
-    splitPageNums = list(map(int,  sys.argv[2:]))
+    splitPageNums = list(map(int, sys.argv[2:]))
+    if splitPageNums[-1] < maxPages:
+      splitPageNums.append(maxPages)
+    splitPageNums = [ (i, splitPageNums[i]) for i in range(0,len(splitPageNums)) ]
   except:
     print('Error: invalid split page number(s).')
 
-  for i, splitPageNum in enumerate(splitPageNums):
+  for i, splitPageNum in splitPageNums:
     if splitPageNum < 1 or splitPageNum > maxPages:
       print('Error: a split page number must be >= 1 and <= %d.' % \
             maxPages)
       sys.exit(3)
-    elif i and splitPageNums[i - 1] >= splitPageNum:
+    elif i and splitPageNums[i - 1][1] >= splitPageNum:
       print('Error: split page numbers must be increasing.')
       sys.exit(4)
 
 baseFN = os.path.splitext(os.path.basename(inputFN))[0]
-pageRect = CGRectMake (0, 0, 612, 792)
-
-if splitPageNums[-1] < maxPages:
-  splitPageNums.append(maxPages)
 
 startPageNum = 1
-for i, splitPageNum in enumerate(splitPageNums):
+for i, splitPageNum in splitPageNums:
   outputFN = '%s.part%d.%d_%d.pdf' % \
-             (baseFN, i + 1, startPageNum, splitPageNum)
-  writeContext = CGPDFContextCreateWithFilename(outputFN, pageRect)
-
+    (baseFN, i + 1, startPageNum, splitPageNum)
   print('Writing page %d-%d to %s...' % \
         (startPageNum, splitPageNum, outputFN))
 
-  for pageNum in range(startPageNum, splitPageNum + 1):
-    mediaBox = inputDoc.getMediaBox(pageNum)
-    writeContext.beginPage(mediaBox)
-    writeContext.drawPDFDocument(mediaBox, inputDoc, pageNum)
-    writeContext.endPage()
+  pages = list(range(startPageNum, splitPageNum + 1))
+  subprocess.run(["pdftk", inputFN, "cat" ] + [ str(x) for x in pages ] + [ "output", outputFN ], stdout=subprocess.PIPE)
 
   startPageNum = splitPageNum + 1
 
